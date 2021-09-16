5e4cdccf1050aacfd5b47dd3e2cd95a577ef2ee3
xunzhao <2201199190@qq.com>
2021-09-16 09:15:02 +0800

    push commit

--- src/main/java/com/git123.java
+++ src/main/java/com/git123.java
@@ -6,5 +6,6 @@ public class git123 {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("push test");
     }
 }
